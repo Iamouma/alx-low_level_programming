678a66d89d88f58de8d6decb10365e532389a600
Iamouma <stephenouma452@gmail.com>
2023-09-07 08:55:26 -0700

    Writes out

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the sentence followed by a new line to the standard error'
+ * Return: 1 if successful
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
+}
