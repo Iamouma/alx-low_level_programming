361e62d9ac8def6d47ba617a15806abf91c4f9e1
Iamouma <stephenouma452@gmail.com>
2023-09-21 06:33:01 -0700

    A function that reverses the content of an array of integers

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * leet - encodes a string into 1337.
+ * @str: string to be encoded.
+ * Return: pointer to the encoded string.
+ */
+char *leet(char *)
+{
+	int i = 0, j;
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+
+	while (str[i])
+	{
+		for (i = 0; j <= 7; j++)
+		{
+			if (str[i] == leet[j] || str[i] - 32 == leet[j])
+				str[i] = j + '0';
+		}
+
+		i++;
+	}
+
+	return (str);
+}
