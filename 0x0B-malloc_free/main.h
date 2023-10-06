f604556c38302da91bd10482085883f4160fc1c4
Iamouma <stephenouma452@gmail.com>
2023-10-06 08:32:29 -0700

    A  function that splits a string into words.

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,5 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+#include <stdlib.h>
+
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
@@ -8,5 +11,8 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
+int len(char *str);
+int num_words(char *str);
+void free_grid(int **grid, int height);
 
 #endif
