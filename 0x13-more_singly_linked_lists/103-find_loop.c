ee1cc4d6608a46a9ecb843af3aeaf1caab66f91f
Iamouma <stephenouma452@gmail.com>
2023-10-24 07:37:33 -0700

    A function that finds the loop in a linked list.

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,34 +1,41 @@
 #include "lists.h"
+
 /**
- * find_listint_loop - finds the loop in a linked list.
- * @head: pointer to the head of a linked list.
- * Return: address of node where loop starts , or NULL if no loop found.
+ * find_listint_loop - Finds the loop contained in
+ *                     a listint_t linked list.
+ * @head: A pointer to the head of the listint_t list.
+ *
+ * Return: If there is no loop - NULL.
+ *         Otherwise - the address of the node where the loop starts.
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *new, *copy = head;
+	listint_t *tortoise, *hare;
 
 	if (head == NULL || head->next == NULL)
 		return (NULL);
 
-	new = new->next;
-	copy = copy->next->next;
+	tortoise = head->next;
+	hare = (head->next)->next;
 
-	while (copy && copy->next)
+	while (hare)
 	{
-		if (new == copy)
-			break;
-		new = new->next;
-		copy = copy->next->next;
+		if (tortoise == hare)
+		{
+			tortoise = head;
+
+			while (tortoise != hare)
+			{
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+
+			return (tortoise);
+		}
+
+		tortoise = tortoise->next;
+		hare = (hare->next)->next;
 	}
 
-	if (new != copy)
-		return (NULL);
-	new = head;
-	while (new != copy)
-	{
-		new = new->next;
-		copy = copy->next;
-	}
-	return (new);
+	return (NULL);
 }
