53a7cc3b0d58dbec195b1bf4d0ff06936865fb5d
Iamouma <stephenouma452@gmail.com>
2023-10-30 13:25:43 -0700

    Creates file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,24 +7,41 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, count = 0;
+	int o;
+	int w;
 
 	if (!filename)
 		return (-1);
 
 	if (!text_content)
-	{
-		for (count = 0; text_content[count];)
-			count++;
-	}
+		text_content = "";
 
 	o = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	w = write(o, text_content, count);
 
-	if (o == -1 || w == -1)
+	if (o == -1)
+		return (-1);
+
+	w = write(o, text_content, _strlen(text_content));
+
+	if (w == -1)
 		return (-1);
 
 	close(o);
 
 	return (1);
 }
+
+/**
+ * strlen - Function that returns the length of string.
+ * @s: pointer to the null-terminated byte string to be examined.
+ * Return: The length of the null-terminated byte string s.
+ */
+int _strlen(const char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+
