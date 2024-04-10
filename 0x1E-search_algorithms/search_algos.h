e207452eb88a118606f239ddab0cb8219dac78db
Iamouma <stephenouma452@gmail.com>
2024-04-10 10:54:48 -0700

    Updated the header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -21,7 +21,6 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-
 /**
  * struct skiplist_s - Singly linked list with an express lane
  *
@@ -51,6 +50,7 @@ int bin_search(int *array, size_t left, size_t right, int value);
 int advanced_binary_recursive(int *array, size_t left,
 				size_t right, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
 
 #endif
